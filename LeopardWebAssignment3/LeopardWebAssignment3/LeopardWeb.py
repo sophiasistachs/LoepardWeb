8fa72aafacb3187633c6d44bfcce0cb5202e0ccb
dovallematwit <117106163+dovallematwit@users.noreply.github.com>
2024-06-03 11:29:23 -0400

    Updated with SQL Commands in class Methods

--- LeopardWebAssignment3/LeopardWebAssignment3/LeopardWeb.py
+++ LeopardWebAssignment3/LeopardWebAssignment3/LeopardWeb.py
@@ -1,4 +1,5 @@
 import sqlite3
+from statistics import quantiles
 
 # database file connection 
 database = sqlite3.connect("LeopardWeb.db") 
@@ -22,12 +23,18 @@ class User:
   #Methods
   def setfirst(self):
     self.name = input("Please enter first name: ")
-
+    sql_command = """UPDATE STUDENT SET NAME = """ + self.name + """ WHERE ID = """ + str(self.ID)
+    cursor.execute(sql_command)
+    
   def setlast(self):
     self.surname = input("Please enter last name: ")
+    sql_command = """UPDATE STUDENT SET SURNAME = """ + self.surname + """ WHERE ID = """ + str(self.ID)
+    cursor.execute(sql_command)
 
   def setid(self):
     self.ID = input("Please enter ID numbr: ")
+    sql_command = """UPDATE STUDENT SET ID = """ + self.ID + """ WHERE NAME = """ + self.name + """, SURNAME = """ + self.surname
+    cursor.execute(sql_command)
 
   def showfirst(self):
     print(self.name)
@@ -41,42 +48,82 @@ class User:
 #Student
 class Student(User):
   #constructer
-  def __init__(self, name, surname, ID):
+  def __init__(self, name, surname, ID, courses = []):
     super().__init__(name, surname, ID)
 
   #Methods
-  def searchcourse(self):
-    course = input("Please enter the name of the course you are searching for: ")
-    print("Here are your results for course",course )
+  def searchcourse(self, course):
+    #course = input("Please enter the name of the course you are searching for: ")
+    try:
+        sql_command = """SELECT TITLE FROM COURSE WHERE TITLE = """ + course
+        cursor.execute(sql_command)
+        return "Course Found"
+    except:
+      return "Course does NOT Exist!!"
 
   def add_drop(self):
     ans = input("Would you like to add or drop a course (select one):")
     if ans == "add":
       course = input("Please enter the name of the course you would like to add: ")
-      print("Course ", course, "has been added ")
+      
+      if (self.searchcourse(course) == "Course Found"):
+        self.courses.append(course)
+        print(course + " was added to your lists of courses")
+      else:
+        print(self.searchcourse)
+      
     elif ans == "drop":
       course = input("Please enter the name of the course you would like to drop: ")
-      print("Course ", course, "has been droped ")
+      
+      if (self.searchcourse(course) == "Course Found"):
+        self.courses.remove(course)
+        print(course + " was removed to your lists of courses")
+      else:
+        print(self.searchcourse + " Therefore could NOT be Removed From your List of Courses")
 
   def show_schedule(self):
-    print("Here is your schedule: \nSCHEDULE")
-
+    print("Schedule:\n-------------------------------------\n")
+    schedule = []
+    for num in len(self.courses):
+      sql_command = """SELECT * FROM COURSE WHERE TITLE = """ + self.courses[num]
+      cursor.execute(sql_command)
+      query_result = cursor.fetchall()
+      #Title, Department, Semester, Year, DayOfWeek, Time
+      schedule[num] = [query_result[1], query_result[2], query_result[5], query_result[6], query_result[4], query_result[3]]
+      print(schedule[num][0] + " at " + schedule[num][1] + " during " + schedule[num][2] + " " + schedule[num][3] + " " + schedule[num][4] + " at " + schedule[num][5])
+      
+      
 #Instructor
 class Instructor(User):
   #Constructer
-  def __init__(self, name, surname, ID):
+  def __init__(self, name, surname, ID, courses = []):
     super().__init__(name, surname, ID)
 
   #Methods
   def show_schedule(self):
-    print("Here is your Instructor schedule: \nSCHEDULE")
+    print("Schedule:\n-------------------------------------\n")
+    schedule = []
+    for num in len(self.courses):
+      sql_command = """SELECT * FROM COURSE WHERE TITLE = """ + self.courses[num]
+      cursor.execute(sql_command)
+      query_result = cursor.fetchall()
+      #Title, Department, Semester, Year, DayOfWeek, Time
+      schedule[num] = [query_result[1], query_result[2], query_result[5], query_result[6], query_result[4], query_result[3]]
+      print(schedule[num][0] + " at " + schedule[num][1] + " during " + schedule[num][2] + " " + schedule[num][3] + " " + schedule[num][4] + " at " + schedule[num][5] + "\n")
 
   def show_classlist(self):
-    print("Here is your classlist: \nCLASSLIST")
-
-  def searchcourse(self):
-    course = input("Please enter the name of the course you are searching for: ")
-    print("Here are your results for course",course )
+    print("Class List:\n-------------------------------------\n")
+    for course in self.courses:
+      print(course + "\n")
+      
+  def searchcourse(self, course):
+    #course = input("Please enter the name of the course you are searching for: ")
+    try:
+        sql_command = """SELECT TITLE FROM COURSE WHERE TITLE = """ + course
+        cursor.execute(sql_command)
+        return "Course Found"
+    except:
+      return "Course does NOT Exist!!"
 
 #Admin
 class Admin(User):
@@ -86,21 +133,76 @@ class Admin(User):
 
   #Methods
   def sys_addCourse(self):
-    course = input("Please enter the name of the course you would like to add to the system: ")
-    print("Course",course, "has been added to the system")
+    crn = input("Please enter the crn of the course you would like to add to the system: ")
+    title = input("Please enter the name of the course: ")
+    department = input("Please enter the department of the course: ")
+    time = input("Please enter the time the course will be taking place: ")
+    day = input("Please enter the day of the week the course will be aviable: ")
+    semester = input("Please enter the semester the course will be aviable: ")
+    year = input("Please enter the year the course will be aviable: ")
+    credit = input("Please enter the number of credits the course is worth: ")
+    
+    sql_command = """INSERT INTO COURSE VALUES(""" + crn + """, """ + title + """, """ + department + """, """ + time + """, """ + day + """, """ + semester + """, """ + year + """, """ + credit + """);"""
+    cursor.execute(sql_command) 
+    print(title + " was Adde to the List of Available Courses")
 
   def sys_dropCourse(self):
     course = input("Please enter the name of the course you would like to drop from the system: ")
-    print("Course",course, "has been dropped to the system")
+    sql_command = """DELETE * FROM COURSE WHERE TITLE = """ + course
+    cursor.execute(sql_command)
+    print(course + " was Deleted From the List of Avaliable Courses\n")
 
   def sys_addremove_user(self):
     ans = input("Would you like to add or remove a user (select one):")
     if ans == "add":
-      course = input("Please enter the name of the user you would like to add: ")
-      print("Course ", course, "has been added ")
+      ID = input("Please enter the ID of the user you would like to add: ")
+      if ID > 40000 and ID < 30000:
+        name = input("Enter new user's name: ")
+        surname = input("Enter new Admin's surname: ")
+        title = input("Enter new Admin's title: ")
+        office = input("Enter new Admin's office: ")
+        email = input("Enter new Admin's email: ")
+        
+        sql_command = """INSERT INTO ADMIN VALUES(""" + ID + """, """ + name + """, """ + surname + """, """ + title + """, """ + office + """, """ + email + """);"""
+        cursor.execute(sql_command)
+        
+      elif ID > 20000:
+        name = input("Enter new Instructor's name: ")
+        surname = input("Enter new Instructor's surname: ")
+        title = input("Enter new Instructor's title: ")
+        hireYear = input("Enter new Instructor's year of hire: ")
+        department = input("Enter new Instructor's department: ")
+        email = input("Enter new Instructor's email: ")
+
+        sql_command = """INSERT INTO INSTRUCTOR VALUES(""" + ID + """, """ + name + """, """ + surname + """, """ + title + """, """ + hireYear + """, """ + department + """, """ + email + """);"""
+        cursor.execute(sql_command) 
+        
+      elif ID > 10000:
+        name = input("Enter new Student's name: ")
+        surname = input("Enter new Student's surname: ")
+        gradYear = input("Enter new Student's expected graduation year: ")
+        major = input("Enter new Student's major: ")
+        email = input("Enter new Student's email: ")
+
+        sql_command = """INSERT INTO STUDENT VALUES(""" + ID + """, """ + name + """, """ + surname + """, """ + gradYear + """, """ + major + """, """ + email + """);"""
+        cursor.execute(sql_command) 
+        
+      else:
+        print("Invalid ID! No User was Removed Due to no User being Selected")
+         
     elif ans == "drop":
-      course = input("Please enter the name of the user you would like to drop: ")
-      print("Course ", course, "has been droped ")
+      ID = input("Please enter the ID of the user you would like to drop: ")
+      if ID > 40000 and ID < 30000:
+        sql_command = """DELETE * FROM ADMIN WHERE ID = """ + ID
+        cursor.execute(sql_command)
+      elif ID > 20000:
+        sql_command = """DELETE * FROM INSTRUCTOR WHERE ID = """ + ID
+        cursor.execute(sql_command)
+      elif ID > 10000:
+        sql_command = """DELETE * FROM STUDENT WHERE ID = """ + ID
+        cursor.execute(sql_command)
+      else:
+        print("Invalid ID! No User was Removed Due to no User being Selected")
 
   def course_addremove_student(self):
     ans = input("Would you like to add or remove a student (select one):")
@@ -132,12 +234,11 @@ else:
   user = "Invalid ID!"
 
 print(user)
-database.close()
 
 #Student Methods
 if user == "Student":
     database = sqlite3.connect("Student.db")
-    sql_command = """SELECT NAME AND SURNAME FROM STUDENT WHERE ID = """ + str(ID)
+    sql_command = """SELECT NAME, SURNAME FROM STUDENT WHERE ID = """ + str(ID)
     cursor.execute(sql_command)
     query_result = cursor.fetchall()
     name = query_result[0]
@@ -158,13 +259,10 @@ if user == "Student":
           p.show_schedule()
 
 
-
-
-
 #Instructor Methods
 elif user == "Instructor":
     database = sqlite3.connect("Instructor.db")
-    sql_command = """SELECT NAME AND SURNAME FROM INSTRUCTOR WHERE ID = """ + str(ID)
+    sql_command = """SELECT NAME, SURNAME FROM INSTRUCTOR WHERE ID = """ + str(ID)
     cursor.execute(sql_command)
     query_result = cursor.fetchall()
     query_result = cursor.fetchall()
@@ -188,11 +286,10 @@ elif user == "Instructor":
           p0.searchcourse()
 
 
-
 #Admin Methods
 elif user == "Admin":
     database = sqlite3.connect("Admin.db")
-    sql_command = """SELECT NAME AND SURNAME FROM ADMIN WHERE ID = """ + str(ID)
+    sql_command = """SELECT NAME, SURNAME FROM ADMIN WHERE ID = """ + str(ID)
     cursor.execute(sql_command)
     query_result = cursor.fetchall()
     name = query_result[0]
@@ -217,6 +314,7 @@ elif user == "Admin":
           p1.course_addremove_student()
         elif task == "5":
           p1.course_roster()
+
 
 database.close()
 database = sqlite3.connect("LeopardWeb.db")
